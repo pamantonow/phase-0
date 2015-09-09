bf1044a3eead8a266a631f824d80c214bacbd323
pamantonow <unstyleurbain@gmail.com>
2015-09-08 22:11:24 -0500

    testing pull request

---
+++ week-4/variables-method.rb
@@ -0,0 +1,14 @@
+# puts "What's your first name"
+# first_name = gets.chomp
+# puts "What's your middle name?"
+# middle_name = gets.chomp
+# puts "What's your last name"
+# last_name = gets.chomp
+
+# puts "Hello #{first_name} #{middle_name} #{last_name}!!"
+
+
+
+puts "What's your favorite number?"
+favorite_number = gets.chomp
+puts "Here is a better number#{favorite_number.to_i + 1}"
