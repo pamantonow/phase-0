89cdb03ff50b702850ac7b8bc4cd43b4cdd0ad9b
Pamela <unstyleurbain@gmail.com>
2015-09-13 22:12:50 -0500

    Update my_solution.rb

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,3 +1,4 @@
+# I worked on this challenge [by myself, with: Julian Leese ].
 def factorial(number)
   total = 1
   until number <= 1
@@ -17,3 +18,13 @@ end
 # number -= 1
 # end while loop
 # return total
+
+#Reflection
+#Writing a method is the cleaner way and perhaps the only way to do a lot in a code.
+#You assigned any declaration to a local variable, or you can do empty strings to an empty array or hash.
+#A method argument is either an array or hash depending of what are you looking for and it's important to have
+#it on your method if you have to do either or.
+#When you return a method it will return whatever the method is doing.
+#When you loop through a hash or an array this means that you are looking for an specific key or number/string 
+#but you have to get access to it that is way looping it's the best way to do it.
+#I paired with Julian and we were both interacting a lot and learning of ways to make our code cleaner.
