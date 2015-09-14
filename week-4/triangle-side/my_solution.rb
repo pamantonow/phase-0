c2cdbbb9be635ad51c932fd5e6d5d7e7319a5ff5
Pamela <unstyleurbain@gmail.com>
2015-09-13 21:34:15 -0500

    Update my_solution.rb

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -2,17 +2,17 @@
 
 
 def valid_triangle?(a,b,c)
-	if a + b <= c
-		return false
-	elsif b + a <= c
-		return false
-	elsif c + a <= b
-		return false
-	elsif c + b <= a
-	return false
-        else 
- 	return true
-       end
+  if a + b <= c
+  return false
+  elsif b + a <= c
+  return false
+  elsif c + a <= b
+  return false
+  elsif c + b <= a
+  return false
+  else 
+ return true
+  end
 end
 
 #refactored solution
