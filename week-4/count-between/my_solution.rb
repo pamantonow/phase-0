ef0f3a6b9f2bfe7c0f81d0db7b603fae74e36644
Pamela <unstyleurbain@gmail.com>
2015-09-13 21:59:30 -0500

    Update my_solution.rb

--- week-4/count-between/my_solution.rb
+++ week-4/count-between/my_solution.rb
@@ -1,10 +1,10 @@
 
 def count_between(num_of_integers,lower_bound,upper_bound)
-	counter = 0
-	num_of_integers.each do |x|
-		if x >= lower_bound && x <= upper_bound
-			counter += 1
-		end
-	end
-	return counter
+ counter = 0
+ num_of_integers.each do |x|
+  if x >= lower_bound && x <= upper_bound
+   counter += 1
+  end
+ end
+return counter
 end
