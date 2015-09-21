d3f633d122ac3ece7405c5b90b3b559c088959b9
Pamela <unstyleurbain@gmail.com>
2015-09-21 10:24:44 -0500

    Update my_solution.rb

--- week-5/calcute-mode/my_solution.rb
+++ week-5/calcute-mode/my_solution.rb
@@ -14,11 +14,11 @@
 def mode(numbers)
  counts = {}
  numbers.each do |num|
- 	if counts.has_key?(num)
- 	counts[num] = counts[num] + 1
- 	else
- 	counts[num] = 1
- 	end
+  if counts.has_key?(num)
+   counts[num] = counts[num] + 1
+   else
+  counts[num] = 1
+  end
  end
 
  num_list = counts.values
@@ -26,9 +26,9 @@ def mode(numbers)
 
  modes = []
  counts.each do |key, count|
- 	if count == highest_count
- 	modes.push(key)
- 	end
+  if count == highest_count
+   modes.push(key)
+  end
  end
  return modes
 end
@@ -38,10 +38,16 @@ def mode(numbers)
   max = 1   
   hash = Hash.new(0)
   numbers.each do |num|
-	  hash[num] += 1
-	end
-	max = hash.values.max
-	new_hash=hash.select { |k,v| v >= max }
-	new_hash.keys
+    hash[num] += 1
+   end
+max = hash.values.max
+new_hash=hash.select { |k,v| v >= max }
+new_hash.keys
 end
- 
+#  Which data structure did you and your pair decide to implement and why?
+#we decided to use new arrays and hashes and push everything to be able to know what is going on
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+#it was a little harder for this since this challenge was a little more complicated than the pad an array.
+# What issues/successes did you run into when translating your pseudocode to code?
+#we actually had just a letter mistake that we forgot in our code but everything was pretty ok!
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
