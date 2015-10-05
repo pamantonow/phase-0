280d0b33c61f3c4a3dcbb374f967aca6dfacb8bd
Pamela <unstyleurbain@gmail.com>
2015-10-05 09:48:06 -0500

    Update nums_commas.js

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -46,15 +46,9 @@ console.log(seperateComma(50000));
 
 
 // Reflection
-function seperateComma(number) {
-	var array = number.toString().split('').reverse();
-	 for (var index = 0; index < array.length; index += 1) {
-	 	if (index % 3 == 0){
-	 		array[index] += ","
-	 	}
-	 }
- array = array.reverse().join("").slice(0,-1);
- return array
-}
-
-console.log(seperateComma(50000));
+//What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+//learning different methods to solve it was tricky since Javascript has some different way to use things.
+//What did you learn about iterating over arrays in JavaScript?
+//learning how to use for properly took me a while to know since in ruby is usually for a different loop.
+//What was different about solving this problem in JavaScript?
+//What built-in methods did you find to incorporate in your refactored solution?
